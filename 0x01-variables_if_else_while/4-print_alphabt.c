bde26c6341240c9f8234ad48d38d9e136a96c3d0
Mbiman <juniormbi80@gmail.com>
2022-07-03 13:22:19 -0500

    mandatory tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int i = 1;
+
+	while (i <= 26)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
