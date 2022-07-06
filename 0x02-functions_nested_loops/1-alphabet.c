274dbafdbf354c371303b4d0cee293f79132474a
Ezembi Godwin Chidiebube <mbiman@localhost.localdomain>
2022-07-05 22:25:11 -0500

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that ptints alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpa++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n')
+}
