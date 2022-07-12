4dc91a84309c8c814b7adc199f2cdea26349a671
Mbiman <juniormbi80@gmail.com>
2022-07-12 23:56:58 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that reset a variable to 98
+ * @n: is a parameter of a function
+ *
+ * Return: returns 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
