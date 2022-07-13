20b0ccc2c1e588b4a62a5699063d24df728b5b5d
Mbiman <juniormbi80@gmail.com>
2022-07-13 01:03:43 +0000

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#insert "main.h"
+/**
+ *
+ * print_rev - a function that prints a string in reverse
+ * @s : a parameter for the print_rev function
+ *
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	int len, x, y;
+
+	y = 0;
+	while (s[y] != '\0')
+	{
+		y++;
+	}
+	for (x = len - 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
