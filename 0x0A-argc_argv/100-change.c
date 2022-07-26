db8126a10e1fdd2bd5a23e0f53829b2cdd42f934
mbiman <juniormbi80@gmail.com>
2022-07-26 16:43:27 -0400

    progress

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a main function that takes argument
+ * @argc: argument count; contains the number of argument passed in the program
+ * @argv: argument vector; one dimensional string array
+ *
+ * Return: integer
+ */
+int main(int argc, char **argv)
+{
+	int sum, count;
+	unsigned int i;
+	char *y;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum = strtol(argv[1], &y, 10);
+	count = 0;
+	if (!*y)
+	{
+		while (sum > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (sum >= cents[i])
+				{
+					count += sum / cents[i];
+					sum = sum % cents[i];
+				}
+			}
+		}
+		if (sum == 1)
+		{
+			count++;
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", count);
+	return (0);
+}
